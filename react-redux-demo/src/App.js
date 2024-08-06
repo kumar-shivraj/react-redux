c80fa9f09ceb8ba6c715ea187e37c985fd205988
kumar-shivraj <shivrajkumar11math@gmail.com>
2024-08-06 16:09:18 +0530

    implement multiple reducer

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -4,11 +4,13 @@ import CakeContainer from "./components/CakeContainer";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import HooksCakeContainer from "./components/HooksCakeContainer";
+import IceCreamContainer from "./components/IceCreamContainer";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <IceCreamContainer />
         <HooksCakeContainer />
         <CakeContainer />
       </div>
